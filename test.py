917950f654bd34856ed1a28c5f76e1030ce60631
Vladislavgocode <bogdanofik@gmail.com>
2022-09-21 14:47:22 +0300

    TEST

--- test.py
+++ test.py
@@ -19,25 +19,20 @@ ffmpeg_options = {'options': "-vn"}
 @client.event
 async def on_message(msg):
     if msg.content.startswith("?play"):
-
         try:
             voice_client = await msg.author.voice.channel.connect()
-            # voice_clients[voice_client.guild.id] = voice_client
         except:
             print("error")
 
         try:
-            # url = msg.content.split()[1]
             string = "test"
             engine = pyttsx3.init()
             engine.save_to_file(string, f'speechers/voice.mp3')
             engine.runAndWait()
-            # f.close()
             voice_client.play(discord.FFmpegPCMAudio(f'speechers/voice.mp3', **ffmpeg_options))
             if voice_client and voice_client.is_connected():
                 await asyncio.sleep(5)
                 await voice_client.disconnect()
-
         except Exception as err:
             print(err)
 
