19e6ec622333071e954916161ef8a954d308692b
Vladislavgocode <bogdanofik@gmail.com>
2022-09-21 16:11:00 +0300

    check

--- test.py
+++ test.py
@@ -18,11 +18,11 @@ ffmpeg_options = {'options': "-vn"}
 async def on_ready():
     await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="Разработчик бота cloudy ы :)"))
 
-cmd = ['ffmpeg', '-i', './assets/voice.mp4', '-vn', '-f', 'mp3', './assets/voice.mp3']
+cmd = ['ffmpeg', '-i', 'speechers/voice.mp3', '-vn', '-f', 'mp3', 'speechers/voice.mp3']
 out = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 print(out.stdout)
 print(out.stderr)
-for f in os.listdir("./assets"):
+for f in os.listdir("./speechers"):
     print(f)
 
 @bot.command()
@@ -30,9 +30,9 @@ async def sgv(ctx):
     voice_client = await ctx.author.voice.channel.connect()
     string = "test"
     engine = pyttsx3.init()
-    engine.save_to_file(string, '/voice.mp3')
+    engine.save_to_file(string, f'speechers/voice.mp3')
     engine.runAndWait()
-    voice_client.play(discord.FFmpegPCMAudio('/voice.mp3', **ffmpeg_options))
+    voice_client.play(discord.FFmpegPCMAudio(f'./speechers/voice.mp3', **ffmpeg_options))
     if voice_client and voice_client.is_connected():
         await asyncio.sleep(5)
         await voice_client.disconnect()
