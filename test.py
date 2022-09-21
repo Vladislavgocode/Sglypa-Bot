f3e6de024aaae136d545f04c2192479714924b2f
Vladislavgocode <bogdanofik@gmail.com>
2022-09-21 13:26:53 +0300

    df

--- test.py
+++ test.py
@@ -31,7 +31,7 @@ async def sgv(ctx):
     else:
         await asyncio.sleep(2)
         voice = await channel.connect()
-        voice.play(discord.FFmpegPCMAudio(executable="sh",source = 'speechers/voice.mp3'))
+        voice.play(discord.FFmpegPCMAudio(executable="sh ffmpeg.exe",source = 'speechers/voice.mp3'))
         if voice and voice.is_connected():
             await asyncio.sleep(5)
             await voice.disconnect()
