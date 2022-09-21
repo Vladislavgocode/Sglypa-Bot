785af230c304a4cbe400b6d63e383847151e21dc
Vladislavgocode <bogdanofik@gmail.com>
2022-09-21 15:06:10 +0300

    test

--- test.py
+++ test.py
@@ -5,6 +5,8 @@ import asyncio
 import time
 import pyttsx3
 
+from sglypa import TOKEN
+
 
 
 
@@ -20,9 +22,9 @@ async def sgv(ctx):
     voice_client = await ctx.author.voice.channel.connect()
     string = "test"
     engine = pyttsx3.init()
-    engine.save_to_file(string, f'speechers/voice.mp3')
+    engine.save_to_file(string, '/voice.mp3')
     engine.runAndWait()
-    voice_client.play(discord.FFmpegPCMAudio(f'speechers/voice.mp3', **ffmpeg_options))
+    voice_client.play(discord.FFmpegPCMAudio('/voice.mp3', **ffmpeg_options))
     if voice_client and voice_client.is_connected():
         await asyncio.sleep(5)
         await voice_client.disconnect()
