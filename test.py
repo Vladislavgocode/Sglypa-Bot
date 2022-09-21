3dbfa68172e6acf901b4d4f7a6f1b199e62b6f6d
Vladislavgocode <bogdanofik@gmail.com>
2022-09-21 14:55:57 +0300

    test

--- test.py
+++ test.py
@@ -1,41 +1,33 @@
-from tokenize import Token
 import discord
 import os
+from discord.ext import commands
 import asyncio
-import youtube_dl
 import time
 import pyttsx3
 
-from sglypa import TOKEN
 
 
 
-client = discord.Client(intents=discord.Intents.all())
+bot = commands.Bot(command_prefix="+",intents=discord.Intents.all())
+bot.remove_command("help")
 TOKEN = os.getenv("DISCORD_TOKEN")
 
 ffmpeg_options = {'options': "-vn"}
 
 
-@client.event
-async def on_message(msg):
-    if msg.content.startswith("?play"):
-        try:
-            voice_client = await msg.author.voice.channel.connect()
-        except:
-            print("error")
-
-        try:
-            string = "test"
-            engine = pyttsx3.init()
-            engine.save_to_file(string, f'speechers/voice.mp3')
-            engine.runAndWait()
-            voice_client.play(discord.FFmpegPCMAudio(f'speechers/voice.mp3', **ffmpeg_options))
-            if voice_client and voice_client.is_connected():
-                await asyncio.sleep(5)
-                await voice_client.disconnect()
-        except Exception as err:
-            print(err)
+@bot.command()
+async def sgv(ctx):
+    voice_client = await ctx.author.voice.channel.connect()
+    string = "test"
+    engine = pyttsx3.init()
+    engine.save_to_file(string, f'speechers/voice.mp3')
+    engine.runAndWait()
+    voice_client.play(discord.FFmpegPCMAudio(f'speechers/voice.mp3', **ffmpeg_options))
+    if voice_client and voice_client.is_connected():
+        await asyncio.sleep(5)
+        await voice_client.disconnect()
 
 
 
-client.run(TOKEN)
+
+bot.run(TOKEN)
